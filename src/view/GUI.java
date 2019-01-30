44367be16f3ba303b133d18c03d7209c41ce46f2
Adefemi Kolawole <akolawole@ggc.edu>
2019-01-30 14:50:43 -0500

    edited url of display location to the right ones

--- src/view/GUI.java
+++ src/view/GUI.java
@@ -1,72 +1,47 @@
 package view;
-
-
-
-
-
-
 import controller.GameController;
 import javafx.application.Application;
 import javafx.scene.Scene;
-
 import javafx.stage.Stage;
 
-
 public class GUI extends Application {
-// try extending to stage
-	//static members
-	private static Stage stage;
-	private static Scene scene;
+    // try extending to stage
+    //static members
+    private static Stage stage;
+    private static Scene scene;
 
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        stage = primaryStage; //link our primaryStage variable to the GUI's primary stage so we can be able to acces it in the contoroller.
+        GameController guiController = new GameController(); //instance of gui controller which controlls all instances of gui-view and classes-model
+        //guiController.setNodesForGui();
+        guiController.setSceneAndStageForGUI(); // this updates GUi accordingly...
+        guiController.getUserInputFromTextField();
+        guiController.setRoomDetails();
+        // show the gui upon running the app
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		stage = primaryStage; //link our primaryStage variable to the GUI's primary stage so we can be able to acces it in the contoroller. 
-		GameController guiController = new GameController(); //instance of gui controller which controlls all instances of gui-view and classes-model
-		//guiController.setNodesForGui();
-		guiController.setSceneAndStageForGUI(); // this updates GUi accordingly...
-		guiController. getUserInputFromTextField();
-		
-		
-		guiController.setRoomDetails();
-		
+    // static getters and setters
+    public static Stage getStage() {
+        return stage;
+    }
 
-		// show the gui upon running the app
-		primaryStage.show();
-
-	}
+    public static void setStage(Stage stage) {
+        GUI.stage = stage;
+    }
 
 
+    public static Scene getScene() {
+        return scene;
+    }
 
+    public static void setScene(Scene scene) {
+        GUI.scene = scene;
+    }
 
-	public static void main(String[] args) {
-		Application.launch(args);
-	}
-
-	// static getters and setters
-	public static Stage getStage() {
-		return stage;
-	}
-
-	public static void setStage(Stage stage) {
-		GUI.stage = stage;
-	}
-	
-	
-	
-	public static Scene getScene() {
-		return scene;
-	}
-
-	public static void setScene(Scene scene) {
-		GUI.scene = scene;
-	}
-
-	
-
-	
-    
-	
-	
 }
